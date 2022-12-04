c564948c1490089b740da28cd649dcdd8e98b247
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2022-12-04 10:00:23 +0700

    Delegasi dan Event
    Menampilkan public relesional pada calculator

--- Calculator.cs
+++ Calculator.cs
@@ -1,4 +1,11 @@
-﻿using System;
+/* 
+Author : Bagus Budi Satoto
+Id     : 21.11.4076
+Class  : 21 IF 04
+
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
